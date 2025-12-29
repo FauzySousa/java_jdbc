bd27f99186adc4470cd6695c56b51b034bdb9390
Fauzy Sousa <denisk9599@gmail.com>
2025-12-29 15:30:01 -0300

    Create usuario.java

---
+++ java_jdbc/src/model/usuario.java
@@ -0,0 +1,42 @@
+package model;
+
+public class usuario {
+    
+    private int codigo;
+    private String nome;
+    private String login;
+    private String password;
+    private String email;
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public int getCodigo() {
+        return codigo;
+    }
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+    public String getLogin() {
+        return login;
+    }
+    public void setLogin(String login) {
+        this.login = login;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+}
